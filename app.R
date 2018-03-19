844ae5a2c433ecfdb53fbad95c968121aed3387b
Paul Jeffries <paul.madison.jeffries@gmail.com>
2018-03-19 00:43:18 -0400

    Update app.R

--- app.R
+++ app.R
@@ -1,7 +1,8 @@
+# import all necessary packages
 library(shiny)
-library(xts)
-library(quantmod)
-library(zoo)
+library(xts) # for xts objects
+library(quantmod) # for the Yahoo finance imports
+library(zoo) # for date rangling
 library(TTR)
 library(magrittr)
 library(htmlwidgets)
@@ -14,11 +15,14 @@ library(shinythemes)
 library(PerformanceAnalytics)
 
 
-# source the Functions.R file
+# source the Functions.R file, where all functions for data importing, cleaning, and vizualization are written
 source("Functions.R")
 
 
-# Define UI for application that draws a histogram
+####################################
+##############   UI   ##############
+####################################
+
 ui <- 
   navbarPage(title = "Asset Comparison Toolbox",
              theme = shinytheme("superhero"),
@@ -27,7 +31,8 @@ ui <-
              tabPanel(title = "Portfolio",
                       sidebarLayout(
                         sidebarPanel(
-                          h5("Welcome! This simple app is meant to allow you to quickly compare the historical performance of various crypto and non-crypto assets."),
+                          h5("Welcome! This app allows you to compare the historical performance of various crypto and non-crypto assets."),
+                          h6("Code and documentation: ", a(href = "https://github.com/pmaji/crypto-asset-comparison-tool/blob/master/README.md", "here on GitHub")),
                           h6("Enter your initial invesment amount ($):"),
                           numericInput(inputId = "initial_investment", 
                                        label = NA, 
@@ -35,15 +40,15 @@ ui <-
                                        min = 1, 
                                        max = NA, 
                                        step = 1),
-                          h6("Use all lower-case to input crypto-assets, and all upper-case to input equities."),
+                          h6("Use lowercase for crypto, and uppercase for all other:"),
                           # first the UI options to pick the 1st asset to compare:
                           h6("Select 1st asset of interest:"),
                           selectInput(inputId = "asset_1a",
-                                         label = NA,
-                                         choices = symbol_list, 
-                                         selected = "eth", 
-                                         multiple = FALSE,
-                                         selectize = TRUE),
+                                      label = NA,
+                                      choices = symbol_list, 
+                                      selected = "eth", 
+                                      multiple = FALSE,
+                                      selectize = TRUE),
                           h6("Select 2nd asset of interest:"),
                           selectInput(inputId = "asset_2a",
                                       label = NA,
@@ -52,87 +57,75 @@ ui <-
                                       multiple = FALSE,
                                       selectize = TRUE),
                           h6("Choose the date range for which you'd like data below:"),
-                          dateRangeInput("port_dates",
+                          dateRangeInput("port_dates1a",
+                                         label = NA, 
+                                         # default date shows past 6 months of data
+                                         start = Sys.Date()-183, 
+                                         end = Sys.Date()-3)
+                        ),
+                        # Show a plot of the generated distribution
+                        mainPanel(
+                          plotlyOutput("portfolio_perf_chart")
+                          
+                        )
+                      )
+             ),
+             
+             # SECOND TAB PAGE BEGINS
+             tabPanel(title = "Returns",
+                      sidebarLayout(
+                        sidebarPanel(
+                          h5("Time for returns"),
+                          h6("Use lowercase for crypto, and uppercase for all other:"),
+                          # first the UI options to pick the 1st asset to compare:
+                          h6("Select 1st asset of interest:"),
+                          selectInput(inputId = "asset_1b",
+                                      label = NA,
+                                      choices = symbol_list, 
+                                      selected = "eth", 
+                                      multiple = FALSE,
+                                      selectize = TRUE),
+                          h6("Select 2nd asset of interest:"),
+                          selectInput(inputId = "asset_2b",
+                                      label = NA,
+                                      choices = symbol_list, 
+                                      selected = "GOOG", 
+                                      multiple = FALSE,
+                                      selectize = TRUE),
+                          h6("Choose the date range for which you'd like data below:"),
+                          dateRangeInput("port_dates1b",
                                          label = NA, 
                                          # default date shows past 6 months of data
                                          start = Sys.Date()-183, 
                                          end = Sys.Date()-3),
-                          wellPanel(
-                            helpText(
-                              a("Read the documentation and methodology here", 
-                                href="https://github.com/pmaji/crypto-asset-comparison-tool/blob/master/README.md", target="_blank")
-                                )
-                              )
-                            ),
-                            # Show a plot of the generated distribution
+                          h6("Choose the period over which to calculate Sharpe ratio:"),
+                          selectInput("period",
+                                      label = NA,
+                                      choices = c("daily", "weekly", "monthly", "quaterly", "yearly"),
+                                      selected = "weekly"),
+                          numericInput(inputId = "Rf", 
+                                       label = NA, 
+                                       value = 0, 
+                                       min = 1, 
+                                       max = NA, 
+                                       step = 0.001),
+                          numericInput(inputId = "p", 
+                                       label = NA, 
+                                       value = 0.95, 
+                                       min = 0.01, 
+                                       max = 0.99, 
+                                       step = 0.001)
+                        ),
+                        # Show a plot of the generated distribution
                         mainPanel(
-                          plotlyOutput("portfolio_perf_chart")
-                                
-                              )
-                            )
-                          ),
-              
-              # SECOND TAB PAGE BEGINS
-              tabPanel(title = "Returns",
-                        sidebarLayout(
-                          sidebarPanel(
-                            h5("Time for returns"),
-                            h6("Use all lower-case to input crypto-assets, and all upper-case to input equities."),
-                            # first the UI options to pick the 1st asset to compare:
-                            h6("Select 1st asset of interest:"),
-                            selectInput(inputId = "asset_1b",
-                                        label = NA,
-                                        choices = symbol_list, 
-                                        selected = "eth", 
-                                        multiple = FALSE,
-                                        selectize = TRUE),
-                            h6("Select 2nd asset of interest:"),
-                            selectInput(inputId = "asset_2b",
-                                        label = NA,
-                                        choices = symbol_list, 
-                                        selected = "GOOG", 
-                                        multiple = FALSE,
-                                        selectize = TRUE),
-                            h6("Choose the date range for which you'd like data below:"),
-                            dateRangeInput("port_dates",
-                                           label = NA, 
-                                           # default date shows past 6 months of data
-                                           start = Sys.Date()-183, 
-                                           end = Sys.Date()-3),
-                            h6("Choose the period over which to calculate Sharpe ratio:"),
-                            selectInput("period",
-                                        label = NA,
-                                        choices = c("daily", "weekly", "monthly", "quaterly", "yearly"),
-                                        selected = "weekly"),
-                            numericInput(inputId = "Rf", 
-                                         label = NA, 
-                                         value = 0, 
-                                         min = 1, 
-                                         max = NA, 
-                                         step = 0.001),
-                            numericInput(inputId = "p", 
-                                         label = NA, 
-                                         value = 0.95, 
-                                         min = 0.01, 
-                                         max = 0.99, 
-                                         step = 0.001),
-                            wellPanel(
-                              helpText(
-                                a("Read the documentation and methodology here", 
-                                  href="https://github.com/pmaji/crypto-asset-comparison-tool/blob/master/README.md", target="_blank")
-                              )
-                            )
-                          ),
-                          # Show a plot of the generated distribution
-                          mainPanel(
-                            plotlyOutput("asset_returns_chart"),
-                            br(),
-                            plotlyOutput("portfolio_sharpe_chart", width = "100%", height = 200)
-                          )
+                          plotlyOutput("asset_returns_chart"),
+                          br(),
+                          plotlyOutput("portfolio_sharpe_chart", width = "100%", height = 200)
                         )
-                        
-                 )
-)
+                      )
+                      
+             )
+  )
 
 
 ####################################
@@ -142,6 +135,7 @@ ui <-
 server <- function(input, output, session) {
   
   # functions to observe changing inputs and constanlty update between tabs
+  # observe and link asset 1
   observe({
     primary_asseta <- input$asset_1a
     updateTextInput(session, "asset_1b", value = primary_asseta)
@@ -151,6 +145,7 @@ server <- function(input, output, session) {
     updateTextInput(session, "asset_1a", value = primary_assetb)
   })
   
+  # observe and link asset 2
   observe({
     secondary_asseta <- input$asset_2a
     updateTextInput(session, "asset_2b", value = secondary_asseta)
@@ -160,6 +155,16 @@ server <- function(input, output, session) {
     updateTextInput(session, "asset_2a", value = secondary_assetb)
   })
   
+  # observe and link date range
+  observe({
+    datea <- input$port_dates1a
+    updateTextInput(session, "port_dates1b", value = datea)
+  })
+  observe({
+    dateb <- input$port_dates1b
+    updateTextInput(session, "port_dates1a", value = dateb)
+  })
+  
   
   
   # 2 step process to create portfolio_perf_chart: create dataset; make viz
@@ -167,20 +172,20 @@ server <- function(input, output, session) {
   output$portfolio_perf_chart <- 
     debounce(
       renderPlotly(
-      {
-        # creates the dataset to feed the viz
-        base_data <- 
-          get_pair_data(
-            asset_1 = input$asset_1a,
-            asset_2 = input$asset_2a, 
-            port_start_date = input$port_dates[1],
-            port_end_date = input$port_dates[2],
-            initial_investment = input$initial_investment
-                       )
-        # builds the actual viz
-        build_portfolio_perf_chart(base_data)
-      }
-            ), millis = 1000) # sets wait time for debounce
+        {
+          # creates the dataset to feed the viz
+          base_data <- 
+            get_pair_data(
+              asset_1 = input$asset_1a,
+              asset_2 = input$asset_2a, 
+              port_start_date = input$port_dates1a[1],
+              port_end_date = input$port_dates1b[2],
+              initial_investment = input$initial_investment
+            )
+          # builds the actual viz
+          build_portfolio_perf_chart(base_data)
+        }
+      ), millis = 1000) # sets wait time for debounce
   
   # 3 step process to create Sharpe ratio chart: create data; derive ratios; make viz
   output$portfolio_sharpe_chart <-
@@ -192,8 +197,8 @@ server <- function(input, output, session) {
             get_pair_data(
               asset_1 = input$asset_1b,
               asset_2 = input$asset_2b, 
-              port_start_date = input$port_dates[1],
-              port_end_date = input$port_dates[2],
+              port_start_date = input$port_dates1a[1],
+              port_end_date = input$port_dates1b[2],
               initial_investment = input$initial_investment
             )
           # builds the altered dataset
@@ -209,7 +214,7 @@ server <- function(input, output, session) {
           sharpe_ratio_plot
           
         }
-    ), millis = 1000)
+      ), millis = 1000)
   
   # 3 step process to create main returns chart: create data, derive ratio, make viz
   output$asset_returns_chart <-
@@ -221,8 +226,8 @@ server <- function(input, output, session) {
             get_pair_data(
               asset_1 = input$asset_1b,
               asset_2 = input$asset_2b, 
-              port_start_date = input$port_dates[1],
-              port_end_date = input$port_dates[2],
+              port_start_date = input$port_dates1a[1],
+              port_end_date = input$port_dates1b[2],
               initial_investment = input$initial_investment
             )
           # builds the altered dataset
@@ -236,7 +241,7 @@ server <- function(input, output, session) {
           
         }
       ), millis = 1000)
-
+  
 }
 
 # Run the application 
