380e75796bc8f841a3a85de40d02507dfa00b836
pmjgit <paul.madison.jeffries@gmail.com>
2019-04-13 20:07:46 -0400

    updated packages and established working director in app.R

--- app.R
+++ app.R
@@ -1,3 +1,6 @@
+# point this to wherever you have cloned this app
+setwd("/Users/pauljeffries/Desktop/personal/personal_code/financial-asset-comparison-tool/")
+
 # import all necessary packages
 library(shiny)
 library(xts) # for xts objects
